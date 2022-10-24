a29660d388c941df26d5277fa876e619ff296bf6
najwanan <najwanan@hotmail.com>
2022-10-23 20:03:25 -0700

    finalizing solutions

--- 10-21-22/app.js
+++ 10-21-22/app.js
@@ -28,8 +28,10 @@ function evenNum(arr, i) {
     const evenArr = arr.filter(x => x % 2 === 0)
     console.log(evenArr, i)
     evenArr.slice(evenArr.length-i, i)
-    console.log(evenArr.splice(0, i))
+    console.log(evenArr.slice(evenArr.length-i))
     console.log('this is a test')
 }
 
 evenNum([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+
+evenNum([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2);
