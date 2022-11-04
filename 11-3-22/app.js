61ac79e1e3346f325b04f473a2329199507f60dc
najwanan <najwanan@hotmail.com>
2022-11-03 23:55:54 -0700

    adding working solution

---
+++ 11-3-22/app.js
@@ -0,0 +1,15 @@
+/*Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+
+last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+last('take me to semynak'), ['take', 'me', 'semynak', 'to'] */
+
+function sortArr(str) {
+str.split(" ").sort((a, b)=> a.CharCodeAt(a.length-1)- b.CharCodeAt(b.length-1))
+}
+
+sortArr()
